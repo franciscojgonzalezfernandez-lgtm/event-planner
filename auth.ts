dc0ac7d8c1bdd4f05471b2d4ac4eaf48a2346393
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2026-04-15 09:29:48 +0200

    Prisma config. Auth with github.

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHub from "next-auth/providers/github";
+import { prisma } from "./lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+});
